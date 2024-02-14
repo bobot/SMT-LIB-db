cca08b083a38339c2e87bb83f5d61c02d543e662
Hans-Joerg Schurr <commits@schurr.at>
2024-02-14 16:24:25 -0600

    Klammerhammer: Add support for compressing the subproblems

--- klhm/src/compress.zig
+++ klhm/src/compress.zig
@@ -3,6 +3,8 @@ const c = @cImport({
     @cInclude("zstd.h");
 });
 
+const data = @import("data.zig");
+
 const Self = @This();
 
 cstream: *c.ZSTD_CStream,
@@ -55,6 +57,74 @@ pub fn compressedSizeSlice(self: *Self, slice: []const u8) !usize {
         if (result == 0 and (inBuffer.pos == inBuffer.size)) {
             return len;
         }
+        outBuffer.pos = 0;
+    }
+    return len;
+}
+
+pub fn compressedSizeIntervalls(self: *Self, slice: []const u8, scopes: *std.ArrayList(data.Scope), checkSat: []const u8) !usize {
+    const r = c.ZSTD_CCtx_reset(self.cstream, c.ZSTD_reset_session_and_parameters);
+    if (c.ZSTD_isError(r) != 0) {
+        return CompressionError.ZSTDError;
+    }
+    var inBuffer = c.ZSTD_inBuffer{
+        .src = @ptrCast(slice),
+        .size = 0,
+        .pos = 0,
+    };
+    var outBuffer = c.ZSTD_outBuffer{
+        .dst = @ptrCast(&self.outBuffer),
+        .size = self.outBuffer.len,
+        .pos = 0,
+    };
+    var len: usize = 0;
+    for (scopes.items) |scope| {
+        var i: usize = 0;
+        while (i < scope.intervals.items.len) : ({
+            i += 2;
+        }) {
+            const start = scope.intervals.items[i];
+            const end = scope.intervals.items[i + 1];
+            inBuffer.pos = start;
+            inBuffer.size = end;
+            while (true) {
+                const result: usize = c.ZSTD_compressStream2(
+                    self.cstream,
+                    &outBuffer,
+                    &inBuffer,
+                    c.ZSTD_e_continue,
+                );
+                if (c.ZSTD_isError(result) != 0) {
+                    return CompressionError.ZSTDError;
+                }
+                len += outBuffer.pos;
+                if (result == 0 and (inBuffer.pos == inBuffer.size)) {
+                    outBuffer.pos = 0;
+                    break;
+                }
+                outBuffer.pos = 0;
+            }
+        }
+    }
+    if (inBuffer.pos != inBuffer.size or outBuffer.pos != 0) {
+        return CompressionError.ZSTDError;
+    }
+    // Flush anything remaining
+    while (true) {
+        inBuffer.src = @ptrCast(checkSat);
+        inBuffer.pos = 0;
+        inBuffer.size = checkSat.len;
+        const result: usize = c.ZSTD_compressStream2(
+            self.cstream,
+            &outBuffer,
+            &inBuffer,
+            c.ZSTD_e_end,
+        );
+        if (c.ZSTD_isError(result) != 0)
+            return CompressionError.ZSTDError;
+        len += outBuffer.pos;
+        if (result == 0 and (inBuffer.pos == inBuffer.size))
+            break;
         outBuffer.pos = 0;
     }
     return len;
